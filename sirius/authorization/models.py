83cf7bfd336ff2c1af2801f7918dde4f3580fe6c
pranav2305 <pranav2305@gmail.com>
2022-03-17 15:11:42 +0530

    Add models

--- sirius/authorization/models.py
+++ sirius/authorization/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from team.models import Team
 
 # Create your models here.
+class Roles(models.Model):
+    role_name = models.CharField(max_length=100)
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
+    role_description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.role_name
+
+class Permissions(models.Model):
+    ACTION_CHOICES = (
+        ('C', 'Create'),
+        ('R', 'Read'),
+        ('U', 'Update'),
+        ('D', 'Delete'),
+    )
+    RELATION_CHOICES = (
+        ('T', 'Team'),
+        ('R', 'Role'),
+        ('P', 'Permission'),
+        ('S', 'Session'),
+        ('M', 'Membership'),
+    )
+    role_id = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    permission_name = models.CharField(max_length=1, choices= ACTION_CHOICES)
+    relation_name = models.CharField(max_length=1, choices= RELATION_CHOICES)
+
+    def __str__(self):
+        return self.permission_name
--- sirius/team/models.py
+++ sirius/team/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from authorization.models import Roles
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+class Membership(models.Model):
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    alumni = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='alumni')
+    role_id = models.ForeignKey(Roles, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
